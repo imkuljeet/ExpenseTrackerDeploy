acfbb3c332b43722991db51fdd0bb8f4af4704f0
imkuljeet <139719986+imkuljeet@users.noreply.github.com>
2024-02-06 01:37:25 +0530

    jenkins checking

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
 
+//jenkins checking
+
 const sequelize = require('./util/database');
 
 const User = require('./models/users');
